5e1e00248bc793d41d1ffd89f366fe22edb8078b
Joshua Baka <fatow.jay@gmail.com>
2025-04-16 13:52:18 +0000

    1-block-scoped modifications

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
 export function taskFirst() {
   const task = 'I prefer const when I can.';
-   return task;
+  return task;
 }
 
 export function getLast() {
@@ -10,5 +10,6 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+  
   return combination;
 }
