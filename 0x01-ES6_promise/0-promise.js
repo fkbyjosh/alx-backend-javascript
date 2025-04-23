a47fb9e0ecdc78d113bf83361c80a55d23f3d103
Joshua Baka <fatow.jay@gmail.com>
2025-04-23 17:40:12 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,11 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     const success = true;
     if (success) {
-      resolve("Promise resolved");
+      resolve('Promise resolved');
     }
     else {
-      reject("Promise rejected");
+      reject('Error');
     }
   });
 }
