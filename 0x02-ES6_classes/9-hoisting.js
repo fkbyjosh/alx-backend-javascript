660a50ab0f4effa8e6243b8f7d3606f475d559f7
Joshua Baka <fatow.jay@gmail.com>
2025-04-29 05:06:11 +0000

    debugging complete

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// First, define the ALXClass before using it
 export class ALXClass {
   constructor(year, location) {
     this._year = year;
@@ -14,12 +13,10 @@ export class ALXClass {
   }
 }
 
-// Now we can create class instances
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
 
-export class StudentALX {
+export class StudentALX extends ALXClass {
   constructor(firstName, lastName, alxClass) {
+    super(alxClass.year, alxClass.location);
     this._firstName = firstName;
     this._lastName = lastName;
     this._alxClass = alxClass;
@@ -37,6 +34,8 @@ export class StudentALX {
     return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
 
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
@@ -44,4 +43,6 @@ const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
+
